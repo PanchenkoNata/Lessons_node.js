aecbc9c1035230a3131eed874345ca243aab64e2
PanchenkoNata <natalitsalucru@gmail.com>
2019-02-11 17:24:28 +0200

    count hash sum of stream, catch errors

--- HW_05_02_19_Promise/hashSum.js
+++ HW_05_02_19_Promise/hashSum.js
@@ -4,16 +4,19 @@ const request = require('request');
 const util = require('util');
 
 const hashOfFile = function(urlInput, urlOutput) {
-    let streamInput = request(urlInput);
+    let streamInput = request(urlInput, (err, response) => {
+        if ( response.statusCode != 200) {
+            throw('File wasn`t found');
+        };
+    });
     let hashSum = crypto.createHash('sha1'); //'sha1' algorithm - consider using sha256
 
     streamInput
         .on('error', () => {
-            console.log("not found");
-            throw('File wasn`t found');
+            throw('Url wasn`t valid');
         })
         .pipe(fs.createWriteStream(urlOutput));
-
+    
     hashSum.setEncoding('hex');
     streamInput.pipe(hashSum);
 
@@ -25,20 +28,40 @@ const hashOfFile = function(urlInput, urlOutput) {
     });
 }
 
-hashOfFile('https://swapi.co/api/people/5/', 'stream_write.txt')
+hashOfFile('https://swapi.co/api/people/5/', 'stream_write3.txt')
     .then((hashData) => {
         console.log('File was upload');
         console.log(`Hash = ${hashData}`);
     })
     .catch((err) => {
         console.log(`You get an error: ${err}`);
-    });
+    })
+    .then(() => {
+        return hashOfFile('https://github.com/anchenkoNata/L_29_11_18_CS-GO/blob/master/index.html', 'stream_write4.txt')
+            .then((hashData) => {
+                console.log('File was upload');
+                console.log(`Hash = ${hashData}`);
+            })
+            .catch((err) => {
+                console.log(`You get an error: ${err}`);
+            });
+    })
+    .then(() => {
+        return hashOfFile('https://github.com/PanchenkoNata/L_29_11_18_CS-GO/blob/master/index.html', 'stream_write4.txt')
+            .then((hashData) => {
+                console.log('File was upload');
+                console.log(`Hash = ${hashData}`);
+            })
+            .catch((err) => {
+                console.log(`You get an error: ${err}`);
+            });
+    })
 
-hashOfFile('https://github.com/PanchenkoNata/L_29_11_18_CS-GO/blob/master/index.html', 'stream_write2.txt')
-    .then((hashData) => {
-        console.log('File was upload');
-        console.log(`Hash = ${hashData}`);
-    })
-    .catch((err) => {
-        console.log(`You get an error: ${err}`);
-    });
+// hashOfFile('https://github.com/anchenkoNata/L_29_11_18_CS-GO/blob/master/index.html', 'stream_write4.txt')
+//     .then((hashData) => {
+//         console.log('File was upload');
+//         console.log(`Hash = ${hashData}`);
+//     })
+//     .catch((err) => {
+//         console.log(`You get an error: ${err}`);
+//     });
