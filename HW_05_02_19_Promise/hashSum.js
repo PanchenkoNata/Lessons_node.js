3fd5d316e8f58c8b1272f27789b41981c274a6f0
PanchenkoNata <natalitsalucru@gmail.com>
2019-02-11 08:10:12 +0200

    count hash sum of stream, 2

--- HW_05_02_19_Promise/hashSum.js
+++ HW_05_02_19_Promise/hashSum.js
@@ -1,9 +1,10 @@
 const fs = require('fs-extra');
 const crypto = require('crypto');
+const request = require('request');
 const util = require('util');
 
 const hashOfFile = function(urlInput, urlOutput) {
-    let streamInput = fs.createReadStream(urlInput, 'utf-8');
+    let streamInput = request(urlInput, 'utf-8');
     let output = fs.createWriteStream(urlOutput);
     const hashSum = crypto.createHash('sha1'); //'sha1' algorithm - consider using sha256
 
@@ -28,7 +29,16 @@ const hashOfFile = function(urlInput, urlOutput) {
     });
 }
 
-hashOfFile('D:/Work_Proga/Procode/HTML_CSS/L_29_11_18_CS-GO/index.html', 'stream_write.txt')
+hashOfFile('https://swapi.co/api/people/5/', 'stream_write.txt')
+    .then((hashData) => {
+        console.log('File was upload');
+        console.log(`Hash = ${hashData}`);
+    })
+    .catch((err) => {
+        console.log(`You get an error: ${err}`);
+    });
+
+hashOfFile('https://github.com/PanchenkoNata/L_29_11_18_CS-GO/blob/master/index.html', 'stream_write2.txt')
     .then((hashData) => {
         console.log('File was upload');
         console.log(`Hash = ${hashData}`);
