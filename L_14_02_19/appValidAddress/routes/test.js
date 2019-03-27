483e1eef5efee5915be37de1e67a06f32fd5651c
PanchenkoNata <natalitsalucru@gmail.com>
2019-03-27 18:45:51 +0200

    appValidAddress fix small mistake

--- L_14_02_19/appValidAddress/routes/test.js
+++ L_14_02_19/appValidAddress/routes/test.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-// router.post('/', (req, res) => {
-//   const address = req.body.inputAddress;
-//   console.log(address);
-//   res.send(`You want to search address: ${address}`);
-// });
+router.post('/', (req, res) => {
+  const address = req.body.inputAddress;
+  console.log(address);
+  res.send(`You want to search address: ${address}`);
+});
 
 module.exports = router;
